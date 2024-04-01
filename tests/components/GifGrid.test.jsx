954a2977cc89342259d6f299b858465108516d7f
Alfonso Rodríguez <alfonsorc@sngular.com>
2024-04-01 17:26:05 +0200

    8.110 test GifGrid. Complete Mock of a custom hook

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,18 +1,43 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('GifGrid tests', () => {
   const category = 'One Punch';
+
   test('must initially show loading', () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
+
     render(<GifGrid category={category} />);
     expect(screen.getByText('Cargando...'))
     expect(screen.getByText(category))
   });
   
   test('must show items when images are loaded with useFetchGifs', () => {
+    const gifs = [
+      {
+        id: 'abc',
+        title: 'Saitama',
+        url: 'foo://'
+      },
+      {
+        id: 'cde',
+        title: 'PI',
+        url: 'foo://pi'
+      },
+
+    ]
+    useFetchGifs.mockReturnValue({
+      images: gifs, 
+      isLoading: false,
+    });
     render(<GifGrid category={category} />);
-    expect(screen.getByText('Cargando...'))
-    expect(screen.getByText(category))
+    expect(screen.getAllByRole('img').length).toBe(2);
   });
 
 });
